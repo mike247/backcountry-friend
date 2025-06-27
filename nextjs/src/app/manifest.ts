ef57f5208769cebd8d089dbc6314ef4475340a18
Michael Norrie <mike.ski247@gmail.com>
2025-06-27 22:01:24 +1200

    pwa

---
+++ nextjs/src/app/manifest.ts
@@ -0,0 +1,24 @@
+import type { MetadataRoute } from "next";
+export default function manifest(): MetadataRoute.Manifest {
+  return {
+    name: "Backcountry Maps",
+    short_name: "BMaps",
+    start_url: "/",
+    display: "standalone",
+    background_color: "#222b38",
+    theme_color: "#222b38",
+    orientation: "any",
+    icons: [
+      {
+        src: "/logo.png",
+        sizes: "192x192",
+        type: "image/png",
+      },
+      {
+        src: "/logo.png",
+        sizes: "512x512",
+        type: "image/png",
+      },
+    ],
+  };
+}
--- nextjs/src/app/page.tsx
+++ nextjs/src/app/page.tsx
@@ -1,7 +1,19 @@
 import Map from "./ui/Map";
 import { getForecast } from "./api/forecast";
+import Head from "next/head";
 
 export default async function Home() {
   const forecast = await getForecast();
-  return <Map forecast={forecast} />;
+  return (
+    <>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="manifest" href="/manifest.webmanifest" />
+        <meta name="theme-color" content="#222b38" />
+        <link rel="apple-touch-icon" href="/logo.png" />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+      </Head>
+      <Map forecast={forecast} />
+    </>
+  );
 }
